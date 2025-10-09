b0c8df8fd9fe89007ff40b98f67d01669a85f85e
Emma Radley <emmaradley@Emmas-MacBook-Air-60.local>
2025-10-09 12:28:56 -0400

    Worked through question 2 and explained my code.

--- project1_emma.py
+++ project1_emma.py
@@ -1,9 +1,9 @@
 #Project 1 Checkpoint
 # Name: Emma Radley
-# Student ID: 
+# Student ID: 8734 4694
 # School Email: emradley@umich.edu
 # Collaborators: Ella Kim
-# USe of AI:
+# Use of AI:
 # Name of the Dataset: Sample Superstore Dataset
 # Columns: Ship Mode, Region, Category, Profit, Sales, Quantity, State, Sub-Category
 # Calculations:
@@ -11,9 +11,59 @@
     # 2. What percentage of Office Supplies that were shipped by First Class - Emma
     # 3. What percentage of Phones(sub-category) sold in California has a higher sales than 300? - Ella
     # 4. What is the average quanitity in each region? - Emma
+
 import kagglehub
+import os
+import csv
 
-# Download latest version
+# Download latest version of the dataset with Kagglehub
 path = kagglehub.dataset_download("bravehart101/sample-supermarket-dataset")
-
 print("Path to dataset files:", path)
+
+#The path to CSV file
+csv_file = os.path.join(path, "SampleSuperstore.csv")
+
+
+#This Reads CSV and returns a list of dictionaries"
+def load_data(csv_file):    
+    data = []
+    with open(csv_file, mode = 'r', newline='', encoding = 'utf-8') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            data.append(row)
+    return data
+
+# QUESTION 2: What percentage of Office Supplies that were shipped by First Class - Emma
+
+#This function filters for rows where the "Category" is "Office Supplies"
+def filter_by_category(data,category):
+    return [row for row in data if row.get('Category') == category]
+
+#This function counts how many of the rows that were office supplies were also shipped by "First Class" for "Ship Mode"
+def count_first_class(filtered_data): 
+    return sum(1 for row in filtered_data if row.get('Ship Mode') == 'First Class')     #For every matching row, it produces the number 1 and creates a sequence, adds up all the ones, and the total is the number of rows that sastifies the "First Class" condition.
+
+
+def calculate_percentage(part,whole):
+    if whole == 0:
+        return 0.0
+    return (part / whole) * 100
+
+#This function prints out what the percentage of office supplies shipped by first class are and ensures that the percentage is a float value rounded to the nearest 2 decimal places.
+def generate_report(percentage):
+    print(f"Percentage of office supplies shipped by first class: {percentage: .2f}%")
+
+def main():
+    data = load_data(csv_file)
+    filtered_data = filter_by_category(data, "Office Supplies")
+    first_class_count = count_first_class(filtered_data)
+    total_office_supplies = len(filtered_data)
+    percentage = calculate_percentage(first_class_count, total_office_supplies)
+    generate_report(percentage)
+
+    #Loads the entire CSV file, Filters rows to just office supplies, counts office supplies shipped by first class, finds total number of office supply rows, calculates the percentage, prints the final result to the user. 
+
+if __name__ == "__main__":
+    main()
+
+
